7c314cf0d0ab31551243021fa1c21c7eff9baf8a
isaacstev <isaacmwongela71@gmail.com>
2024-10-07 11:48:08 +0300

    Project setup complete

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';  // Updated path to reflect 'styles' subfolder
+import './styles/index.css';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
